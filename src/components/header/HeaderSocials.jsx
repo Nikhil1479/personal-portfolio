4c1458d5ba0329f124c636a46e1f7ed6c947e477
Nikhil Gupta <80402226+Nikhil1479@users.noreply.github.com>
2025-11-20 17:22:10 +0530

    Add rel="noreferrer" attribute to social media links for security

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -4,10 +4,10 @@ import { BsLinkedin, BsGithub } from "react-icons/bs";
 
 const HeaderSocials = () => {
   return (
-      <div className='header__socials'>
-          <a href="https://linkedin.com" target='_blank'><BsLinkedin/></a>
-          <a href="https://github.com" target='_blank'><BsGithub/></a>
-          <a href="https://linkedin.com" target='_blank'><BsLinkedin/></a>
+    <div className='header__socials'>
+      <a href="https://linkedin.com" target='_blank' rel="noreferrer"><BsLinkedin /></a>
+      <a href="https://github.com" target='_blank' rel="noreferrer"><BsGithub /></a>
+      <a href="https://linkedin.com" target='_blank' rel="noreferrer"><BsLinkedin /></a>
     </div>
   )
 }
